2df6fc65829a6a348eab756b859e67151a9c2449
rezwan-raj <rezwanahmedcse@gmail.com>
2025-12-02 18:55:48 +0600

    create Login repository

---
+++ Learnig code/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    System.out.println("Enter any key : ");
+    String ch = sc.next();
+      String d = "codingburg";
+      while (true){
+          if (ch.equals("a")) {
+              System.out.println("Sorder saikat");
+          }else if(ch.equals("b")){
+              System.out.println("Jemmy Apu");
+          }else if(ch.equals("c")){
+              System.out.println("Rezwan Ahmed Raj");
+          }else if (ch =="d"){
+              System.out.println("CodingBurg "+d);
+          }else if(ch=="e"){
+              System.out.println("Happy");
+          }
+
+      }
+
+}
+      }
---
+++ Learnig code/positiveNegcheck.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+public class positiveNegcheck {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+    }
+}
